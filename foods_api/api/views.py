8da16775eba0dcf643dcb0d269a120e46286d591
Ilya Bagrov <ilya.b.only@gmail.com>
2024-04-16 18:21:41 +0300

    Add food filter

--- foods_api/api/views.py
+++ foods_api/api/views.py
@@ -1,8 +1,11 @@
+from django.db.models import Prefetch
 from rest_framework import generics
 
-from api.models import FoodCategory, FoodListSerializer
+from api.models import Food, FoodCategory, FoodListSerializer
 
 
 class FoodCategoriesAPIView(generics.ListAPIView):
-    queryset = FoodCategory.objects.all()
+    prefetch = Prefetch('food', queryset=Food.objects.filter(is_publish=True))
+    queryset = FoodCategory.objects.prefetch_related(prefetch). \
+        filter(food__is_publish=True).distinct()
     serializer_class = FoodListSerializer
