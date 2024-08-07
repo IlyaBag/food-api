1009939c918645924f547ae23b76cf4f98decf0b
Ilya Bagrov <ilya.b.only@gmail.com>
2024-08-07 19:33:31 +0300

    Add docstring to FoodCategoriesAPIView

--- foods_api/api/views.py
+++ foods_api/api/views.py
@@ -5,6 +5,10 @@ from api.models import Food, FoodCategory, FoodListSerializer
 
 
 class FoodCategoriesAPIView(generics.ListAPIView):
+    """
+    Shows food categories (with embedded foods) that strictly contain food with 
+    the attribute 'is_publish == True'.
+    """
     prefetch = Prefetch('food', queryset=Food.objects.filter(is_publish=True))
     queryset = FoodCategory.objects.prefetch_related(prefetch). \
         filter(food__is_publish=True).distinct()
