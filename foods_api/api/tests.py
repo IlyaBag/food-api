66d7239d091bc3e0771f218c7e5862b494d84ec3
Ilya Bagrov <ilya.b.only@gmail.com>
2024-08-08 03:05:57 +0300

    Add unit test for FoodCategoriesAPIView

--- foods_api/api/tests.py
+++ foods_api/api/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class FoodCategoriesAPIViewTest(TestCase):
+    """Testing API views."""
+    fixtures = ['cats_and_foods.json']
+
+    def test_get_categories_with_published_food(self):
+        response = self.client.get(reverse('food_categories'))
+        self.assertEqual(response.status_code, 200)
+
+        with open('api/fixtures/expected.json', 'r') as file:
+            # Format expected JSON to oneline string
+            expected = ''.join(list(map(lambda line: line.strip(), file.readlines()))).replace(': ', ':')
+        self.assertContains(response, expected)
